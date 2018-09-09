6ada98ef3f7c4b4377e98250d457493068a76ff7
nischal basuti <nischalbasuti1@gmail.com>
2018-09-09 23:14:13 +0700

    Added drop down to quotations.

--- app/controllers/ps2_controller.rb
+++ app/controllers/ps2_controller.rb
@@ -3,8 +3,17 @@ class Ps2Controller < ApplicationController
   end
 
   def quotation
+    # Get all distinct categories.
+    @category_options = Quotation.distinct.pluck(:category).map do |category|
+      [category, category]
+    end
+
+    # Create a new quotation if passed.
     if params[:quotation]
       @quotation = Quotation.new( params.require(:quotation).permit(:author_name, :category, :quote) )
+      if params[:new_category].strip != ""
+        @quotation[:category] = params[:new_category]
+      end
       if @quotation.save
         flash[:notice] = 'Quotation was successfully created.'
         @quotation = Quotation.new
@@ -12,6 +21,8 @@ class Ps2Controller < ApplicationController
     else
       @quotation = Quotation.new
     end
+
+    # Order of quotations.
     if params[:sort_by] == "date"
       @quotations = Quotation.order(:created_at)
     else
