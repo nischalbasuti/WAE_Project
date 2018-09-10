12fd770e0cc073b194fe184dd99571cc47585d82
nischal basuti <nischalbasuti1@gmail.com>
2018-09-11 01:07:16 +0700

    Implemented killing quotes

--- app/controllers/ps2_controller.rb
+++ app/controllers/ps2_controller.rb
@@ -2,12 +2,45 @@ class Ps2Controller < ApplicationController
   def index
   end
 
+  def kill_quote(id)
+    id = id.to_i
+    if cookies[:dead_quotes]
+      dead_quotes = JSON.parse cookies[:dead_quotes]
+    else
+      dead_quotes = []
+    end
+    dead_quotes.push(id)
+    dead_quotes = dead_quotes.uniq
+    cookies[:dead_quotes] = JSON.generate(dead_quotes)
+  end
+
+  def get_dead_quotes()
+    if cookies[:dead_quotes]
+      return JSON.parse cookies[:dead_quotes]
+    else
+      return []
+    end
+  end
+
   def quotation
+    logger = Logger.new(STDOUT)
+
+    if params[:revive_quote]
+      cookies[:dead_quotes] = JSON.generate []
+    end
+
     # Get all distinct categories.
     @category_options = Quotation.distinct.pluck(:category).map do |category|
       [category, category]
     end
     
+    if params[:kill_quote]
+      logger.info "KILLING QUOTE "+params[:kill_quote]
+      kill_quote(params[:kill_quote])
+    end
+
+    @dead_quotes = get_dead_quotes
+
     # Search for words in autor and quote fields using regex in the SQL.
     @search_query = ""
     @original_query = ""
@@ -18,19 +51,15 @@ class Ps2Controller < ApplicationController
     end
 
     if params[:sort_by] == "date"
-      @quotations = Quotation.where("LOWER(author_name) ~* ?
-                                    or LOWER(quote) ~* ?",
-                                    @search_query, @search_query)
+      @quotations = Quotation.where("LOWER(author_name) ~* ?  or LOWER(quote) ~* ?",
+                                    @search_query, @search_query).where.not(id: get_dead_quotes)
                                     .order(:created_at)
     else
-      @quotations = Quotation.where("LOWER(author_name) ~* ?
-                                    or LOWER(quote) ~* ?",
-                                    @search_query, @search_query)
+      @quotations = Quotation.where("LOWER(author_name) ~* ?  or LOWER(quote) ~* ?",
+                                    @search_query, @search_query).where.not(id: get_dead_quotes)
                                     .order(:category)
     end
-    
-    logger = Logger.new(STDOUT)
-    logger.info @search_query
+    logger.info get_dead_quotes
 
     # Create a new quotation if passed.
     if params[:quotation]
