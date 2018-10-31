67bfc9aa24cf12e8907f5319cc3aebf58306d908
nischal basuti <nischalbasuti1@gmail.com>
2018-10-31 19:18:57 +0700

    added cascade deletion

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,4 @@
 class Activity < ApplicationRecord
   belongs_to :event
-  has_many :requirements
+  has_many :requirements, dependent: :destroy
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
-  has_many :activities
-  has_many :forums
+  has_many :activities, dependent: :destroy
+  has_many :forums, dependent: :destroy
+  has_many :user_events, dependent: :destroy
 end
