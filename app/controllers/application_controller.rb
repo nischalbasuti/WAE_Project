146002fa59dd2ed5829616681a15cb9065f87140
nischal basuti <nischalbasuti1@gmail.com>
2018-10-31 18:42:41 +0700

    created 403 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error]  = "Access denied."
+    # render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
     redirect_to root_url
   end
 end
