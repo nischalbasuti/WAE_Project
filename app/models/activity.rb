1dad4b60c35ca381759180723f4c2b02b5bbd9e8
nischal basuti <nischalbasuti1@gmail.com>
2018-11-18 21:21:52 +0700

    Validating activity dates on creation.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,7 @@ class Activity < ApplicationRecord
   has_many :requirements, dependent: :destroy
 
   before_save :validate_dates
+  before_create :validate_dates
 
   def valid_dates?
     # Checking (1) if the activites start and end time lie between the duration
